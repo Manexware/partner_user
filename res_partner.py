f3d54ea32bffdb506cf70d93e54aa715492878b1
Luis Pérez <enrique.pere@manexware.com>
2016-10-12 11:48:34 -0500

    [FIX] Ruc

--- res_partner.py
+++ res_partner.py
@@ -6,13 +6,13 @@
 import re
 import unicodedata
 try:
-    from openerp.osv import osv, fields
+    from openerp import models, fields
     from openerp import SUPERUSER_ID
     from openerp.tools.translate import _
     from openerp.tools import email_split, single_email_re
     from openerp.tools import ustr
 except:
-    from odoo.osv import osv, fields
+    from odoo import models, fields
     from odoo import SUPERUSER_ID
     from odoo.tools.translate import _
     from odoo.tools import email_split, single_email_re
@@ -38,45 +38,43 @@ def remove_accents(input_str):
     return u''.join([c for c in nkfd_form if not unicodedata.combining(c)])
 
 
-class partner(osv.osv):
+class partner(models.Model):
 
     """"""
 
     _inherit = 'res.partner'
 
-    def _retrieve_user(self, cr, uid, ids, arg, karg, context=None):
+    def _retrieve_user(self, context=None):
         """ retrieve the (possibly inactive) user corresponding to wizard_user.partner_id
             @param wizard_user: browse record of model portal.wizard.user
             @return: browse record of model res.users
         """
         context = dict(context or {}, active_test=False)
-        res_users = self.pool.get('res.users')
+        res_users = self.env['res.users']
         res = {}
-        for i in ids:
+        for i in self:
             domain = [('partner_id', '=', i)]
-            user_ids = res_users.search(cr, uid, domain, context=context)
+            user_ids = res_users.search(domain)
             user_id = False
             if user_ids:
                 user_id = user_ids[0]
             res[i] = user_id
         return res
 
-    _columns = {
-        'login': fields.related('related_user_id', 'login', string='Login', type='char', size=64, readonly=True,
-                                help="Used to log into the system"),
-        'password': fields.related('related_user_id', 'password', string='Password', type='char', size=64, readonly=True,
-                                   help="Keep empty if you don't want the user to be able to connect on the system."),
-        'related_user_id': fields.function(_retrieve_user, relation='res.users', string='User', type='many2one', ),
-        'template_user_id': fields.many2one('res.users', string="Template User", domain=[('active', '=', False)],),
-    }
+    login = fields.Char(related='user_id.login', string='Login',
+                            help="Used to log into the system", store=True)
+    # password = fields.Char(related='related_user_id.password', string='Password', size=64, readonly=True,
+    #                            help="Keep empty if you don't want the user to be able to connect on the system.")
+    related_user_id = fields.Many2one('res.users', compute='_retrieve_user', string='User', type='many2one',store=True)
+    template_user_id = fields.Many2one('res.users', string="Template User", domain=[('active', '=', False)])
 
     def open_related_user(self, cr, uid, ids, context=None):
-        user_id = self.browse(
-            cr, uid, ids[0], context=context).related_user_id.id
+        # user_id = self.browse(
+        #     cr, uid, ids[0], context=context).related_user_id.id
+        user_id = self.related_user_id.id
         if not user_id:
             return False
-        view_ref = self.pool.get('ir.model.data').get_object_reference(
-            cr, uid, 'base', 'view_users_form')
+        view_ref = self.env['ir.model.data'].get_object_reference('base', 'view_users_form')
         view_id = view_ref and view_ref[1] or False,
         return {
             'type': 'ir.actions.act_window',
@@ -88,45 +86,46 @@ class partner(osv.osv):
             # 'flags': {'form': {'action_buttons': True, 'options': {'mode': 'edit'}}}
         }
 
-    def delete_user(self, cr, uid, ids, context=None):
-        user_id = self.browse(
-            cr, uid, ids[0], context=context).related_user_id.id
+    def delete_user(self):
+        # user_id = self.browse(
+        #     cr, uid, ids[0], context=context).related_user_id.id
+        user_id = self.related_user_id.id
         if not user_id:
             return False
-        return self.pool.get('res.users').unlink(cr, uid, [user_id], context=context)
+        return self.env['res.users'].unlink([user_id])
 
-    def retrieve_user(self, cr, uid, partner, context=None):
+    def retrieve_user(self,partner):
         """ retrieve the (possibly inactive) user corresponding to partner
             @param partner: browse record of model portal.wizard.user
             @return: browse record of model res.users
         """
 
-        context = dict(context or {}, active_test=False)
-        res_users = self.pool.get('res.users')
+        # context = dict(context or {}, active_test=False)
+        res_users = self.env['res.users']
         domain = [('partner_id', '=', partner.id)]
-        user_ids = res_users.search(cr, uid, domain, context=context)
+        user_ids = res_users.search(domain)
         return user_ids
 
-    def quickly_create_user(self, cr, uid, ids, context=None):
-        res_users = self.pool.get('res.users')
+    def quickly_create_user(self):
+        res_users = self.env['res.users']
         # Make this an option
-        context = dict(context or {}, no_reset_password=True)
+        # context = dict(context or {}, no_reset_password=True)
         # TODO Pasar argumentos para activar o desactivar
         create_user = True
 
-        for partner in self.browse(cr, SUPERUSER_ID, ids, context):
+        for partner in self:
             group_ids = []
             # if not partner.template_user_id:
             #     raise osv.except_osv(_('Non template user selected!'),
             #                          _('Please define a template user for this partner: "%s" (id:%d).') % (partner.name, partner.id))
             # group_ids = [x.id for x in partner.template_user_id.groups_id]
-            user_ids = self.retrieve_user(cr, SUPERUSER_ID, partner, context)
+            user_ids = self.retrieve_user(partner)
             if create_user:
                 # create a user if necessary, and make sure it is in the portal
                 # group
                 if not user_ids:
                     user_ids = [
-                        self._create_user(cr, SUPERUSER_ID, partner, context)]
+                        self._create_user(partner)]
                 # res_users.write(
                 #     cr, SUPERUSER_ID, user_ids, {'active': True, 'groups_id': [(6, 0, group_ids)]})
                 # prepare for the signup process
@@ -136,22 +135,21 @@ class partner(osv.osv):
                 # self._send_email(cr, uid, partner, context)
             elif user_ids:
                 # deactivate user
-                res_users.write(cr, SUPERUSER_ID, user_ids, {'active': False})
+                res_users.write(user_ids, {'active': False})
 
-    def _create_user(self, cr, uid, partner, context=None):
+    def _create_user(self):
         """ create a new user for partner.partner_id
             @param partner: browse record of model partner.user
             @return: browse record of model res.users
         """
-        res_users = self.pool.get('res.users')
+        res_users = self.env['res.users']
         # to prevent shortcut creation
         create_context = dict(
-            context or {}, noshortcut=True, no_reset_password=True, signup_valid=True,create_user=True)
+            self.contex or {}, noshortcut=True, no_reset_password=True, signup_valid=True,create_user=True)
         if partner.email and single_email_re.match(partner.email):
             login = extract_email(partner.email)
         else:
-            login = self._clean_and_make_unique(
-                cr, uid, partner.name, context=context)
+            login = self._clean_and_make_unique(partner.name)
         values = {
             # 'email': extract_email(partner.email),
             'login': login,
@@ -161,19 +159,19 @@ class partner(osv.osv):
             'company_id': partner.company_id.id,
             'company_ids': [(4, partner.company_id.id)],
         }
-        user = res_users.create(cr, uid, values, context=create_context)
+        user = res_users.create(values, context=create_context)
         if user:
-            res_users.action_reset_password(cr, uid, user, context=create_context)
+            res_users.action_reset_password(user, context=create_context)
         return
 
-    def _clean_and_make_unique(self, cr, uid, name, context=None):
+    def _clean_and_make_unique(self, name, context=None):
         # when an alias name appears to already be an email, we keep the local
         # part only
         name = remove_accents(name).lower().split('@')[0]
         name = re.sub(r'[^\w+.]+', '.', name)
-        return self._find_unique(cr, uid, name, context=context)
+        return self._find_unique(name, context=context)
 
-    def _find_unique(self, cr, uid, name, context=None):
+    def _find_unique(self, name, context=None):
         """Find a unique alias name similar to ``name``. If ``name`` is
            already taken, make a variant by adding an integer suffix until
            an unused alias is found.
@@ -182,7 +180,7 @@ class partner(osv.osv):
         while True:
             new_name = "%s%s" % (
                 name, sequence) if sequence is not None else name
-            if not self.pool.get('res.users').search(cr, uid, [('login', '=', new_name)]):
+            if not self.env['res.users'].search([('login', '=', new_name)]):
                 break
             sequence = (sequence + 1) if sequence else 2
         return new_name
